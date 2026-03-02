ed7cfdeaeed05e896c03bfb9a58f9b08060596a7
Ssalamancag10 <gamboa9810@gmail.com>
2026-03-02 13:14:48 -0500

    Update pipeline.py

--- data/python/pipeline.py
+++ data/python/pipeline.py
@@ -1,36 +1,17 @@
 import pandas as pd
 
-# 1. Load raw data
 df = pd.read_csv("raw/northwind_raw.csv")
 
-# 2. Basic validation
-#print("Rows:", len(df))#Cuantas filas hay
-#print(df.head())#Primeras 5 filas
-
-# 3. Data types
-#print(df.dtypes)#Tipo de variable de las columnas
-
-# 4. Convert OrderDate to datetime
-df["OrderDate"] = pd.to_datetime(df["OrderDate"])#Cambia tipo a fecha
-
-# 5. Check result
-#print(df["OrderDate"].dtype)
-
-# 6. Create analytical date columns
+df["OrderDate"] = pd.to_datetime(df["OrderDate"])
 df["Year"] = df["OrderDate"].dt.year
 df["Month"] = df["OrderDate"].dt.month
 df["YearMonth"] = df["OrderDate"].dt.to_period("M").astype(str)
 
-#print(df[["OrderDate", "Year", "Month", "YearMonth"]].head())
-
-# 7. Data quality checks Manejo de errores
 assert df["Quantity"].min() > 0, "Quantity has invalid values"
 assert df["Price"].min() > 0, "Price has invalid values"
 assert df["LineTotal"].sum() > 0, "Total sales is zero"
-
-# 8. Save cleaned data
 df.to_csv("processed/sales_cleaned.csv", index=False)
-# 9. Monthly sales metrics
+
 monthly_sales = (
     df.groupby("YearMonth")
     .agg(
@@ -40,9 +21,8 @@ monthly_sales = (
     )
     .reset_index()
 )
-
 print(monthly_sales.head())
-# 10. Product sales metrics
+
 product_sales = (
     df.groupby(["ProductID", "ProductName"])
     .agg(
@@ -52,9 +32,8 @@ product_sales = (
     .reset_index()
     .sort_values("total_sales", ascending=False)
 )
-
 print(product_sales.head())
-# 11. Customer sales metrics
+
 customer_sales = (
     df.groupby(["CustomerID", "CustomerName"])
     .agg(
@@ -64,10 +43,8 @@ customer_sales = (
     .reset_index()
     .sort_values("total_sales", ascending=False)
 )
-
 print(customer_sales.head())
 
-# 12. Save analytics datasets
 monthly_sales.to_csv("analytics/monthly_sales.csv", index=False)
 product_sales.to_csv("analytics/product_sales.csv", index=False)
 customer_sales.to_csv("analytics/customer_sales.csv", index=False)
